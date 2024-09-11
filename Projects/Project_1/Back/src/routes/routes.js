fe3065be386c66faa510d928c355e0962e4d3ef0
solmaznsr <67147501+solmaznsr@users.noreply.github.com>
2024-09-11 04:55:44 -0800

    fix: fix routes file for submit

--- Projects/Project_1/Back/src/routes/routes.js
+++ Projects/Project_1/Back/src/routes/routes.js
@@ -1,19 +1,8 @@
 import { readData, createRecord } from '../utils/files.js';
+import { submitData } from './submitData.js';
 
 export default async function routes(fastify) {
-
-    fastify.post('/api/submit', async(request, reply) => {
-        try {
-            const data = request.body;
-            let jsonData = readData();
-            jsonData.push(data);
-            createRecord(jsonData);
-            reply.code(200).send({ message: 'Data saved successfully' });
-        } catch (error) {
-            console.error('Error processing request:', error);
-            reply.code(500).send({ message: 'Error processing request' });
-        }
-    });
+    fastify.post('/api/submit', submitData);
 
     fastify.get('/api/data', async(request, reply) => {
         try {
