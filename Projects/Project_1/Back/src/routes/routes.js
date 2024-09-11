de353ca846947c891c9dffaf12a8dee9e2440b4b
solmaznsr <67147501+solmaznsr@users.noreply.github.com>
2024-09-11 05:05:23 -0800

    fix: fix routes file for get data

--- Projects/Project_1/Back/src/routes/routes.js
+++ Projects/Project_1/Back/src/routes/routes.js
@@ -1,55 +1,12 @@
 import { readData, createRecord } from '../utils/files.js';
 import { submitData } from './submitData.js';
+import { getDataById } from './getDataById.js';
+import { updateDataById } from '../functions/updateDataById.js';
 
 export default async function routes(fastify) {
     fastify.post('/api/submit', submitData);
-
-    fastify.get('/api/data', async(request, reply) => {
-        try {
-            const jsonData = readData();
-            reply.code(200).send(jsonData);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-            reply.code(500).send({ message: 'Error fetching data' });
-        }
-    });
-
-    fastify.get('/api/data/:id', async(request, reply) => {
-        try {
-            const id = Number(request.params.id);
-            const jsonData = readData();
-            const item = jsonData.find((entry, index) => index === id);
-
-            if (!item) {
-                return reply.code(404).send({ message: 'Data not found' });
-            }
-
-            reply.code(200).send(item);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-            reply.code(500).send({ message: 'Error fetching data' });
-        }
-    });
-
-    fastify.put('/api/data/:id', async(request, reply) => {
-        try {
-            const id = Number(request.params.id);
-            const updatedData = request.body;
-            let jsonData = readData();
-
-            if (id >= jsonData.length) {
-                return reply.code(404).send({ message: 'Data not found' });
-            }
-
-            jsonData[id] = updatedData;
-            createRecord(jsonData);
-
-            reply.code(200).send({ message: 'Data updated successfully' });
-        } catch (error) {
-            console.error('Error updating data:', error);
-            reply.code(500).send({ message: 'Error updating data' });
-        }
-    });
+    fastify.get('/api/data/:id', getDataById);
+    fastify.put('/api/data/:id', updateDataById);
 
     fastify.delete('/api/data/:id', async(request, reply) => {
         try {
