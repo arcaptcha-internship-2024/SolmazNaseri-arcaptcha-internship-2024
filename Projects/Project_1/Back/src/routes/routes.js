2a95a72550d5435c7aca026e6efeb98225bbda5c
solmaznsr <67147501+solmaznsr@users.noreply.github.com>
2024-09-11 05:07:49 -0800

    fix: fix routes file for delete data

--- Projects/Project_1/Back/src/routes/routes.js
+++ Projects/Project_1/Back/src/routes/routes.js
@@ -1,29 +1,15 @@
 import { readData, createRecord } from '../utils/files.js';
 import { submitData } from './submitData.js';
 import { getDataById } from './getDataById.js';
-import { updateDataById } from '../functions/updateDataById.js';
+import { updateDataById } from './updateDataById.js';
+import { deleteDataById } from './deleteDataById.js';
 
 export default async function routes(fastify) {
     fastify.post('/api/submit', submitData);
     fastify.get('/api/data/:id', getDataById);
     fastify.put('/api/data/:id', updateDataById);
+    fastify.delete('/api/data/:id', deleteDataById);
 
-    fastify.delete('/api/data/:id', async(request, reply) => {
-        try {
-            const id = Number(request.params.id);
-            let jsonData = readData();
 
-            if (id >= jsonData.length) {
-                return reply.code(404).send({ message: 'Record not found' });
-            }
 
-            jsonData.splice(id, 1);
-            createRecord(jsonData);
-
-            reply.code(200).send({ message: 'Record deleted successfully' });
-        } catch (error) {
-            console.error('Error deleting record:', error);
-            reply.code(500).send({ message: 'Error deleting record' });
-        }
-    });
 }
