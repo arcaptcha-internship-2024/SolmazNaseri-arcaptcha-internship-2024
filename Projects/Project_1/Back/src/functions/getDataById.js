37be2bc7ea7059ee42052f2d606710779d2c7d09
solmaznsr <67147501+solmaznsr@users.noreply.github.com>
2024-09-12 10:15:48 -0800

    fix: fix get data function for storage

--- Projects/Project_1/Back/src/functions/getDataById.js
+++ Projects/Project_1/Back/src/functions/getDataById.js
@@ -1,10 +1,22 @@
 import { readData } from '../utils/files.js';
 
+async function getJsonDataById(id) {
+    const jsonData = readData();
+    return jsonData.find((entry, index) => index === id);
+}
+
+const storage = {
+    json: getJsonDataById,
+};
+
 export async function getDataById(request, reply) {
     try {
         const id = Number(request.params.id);
-        const jsonData = readData();
-        const item = jsonData.find((entry, index) => index === id);
+        const { sourceType = 'json' } = request.query || {};
+        if (!storage[sourceType]) {
+            return reply.code(400).send({ message: `Storage source ${sourceType} not supported` });
+        }
+        const item = await storage[sourceType](id);
 
         if (!item) {
             return reply.code(404).send({ message: 'Data not found' });
