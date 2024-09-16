b89faaacf54587532024200fab8dfc0c03e3d067
solmaznsr <67147501+solmaznsr@users.noreply.github.com>
2024-09-16 01:13:11 -0800

    fix:fix docker bug

---
+++ Projects/Project_1/Back/app/functions/deleteDataById.js
@@ -0,0 +1,20 @@
+import { readData, createRecord } from '../utils/files.js';
+
+export async function deleteDataById(request, reply) {
+    try {
+        const id = Number(request.params.id);
+        let jsonData = readData();
+
+        if (id >= jsonData.length) {
+            return reply.code(404).send({ message: 'Record not found' });
+        }
+
+        jsonData.splice(id, 1);
+        createRecord(jsonData);
+
+        reply.code(200).send({ message: 'Record deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting record:', error);
+        reply.code(500).send({ message: 'Error deleting record' });
+    }
+}
