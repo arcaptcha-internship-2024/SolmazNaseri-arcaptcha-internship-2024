d173184ea423d3f8e91c4188a0914e2dc8b7dd0f
solmaznsr <67147501+solmaznsr@users.noreply.github.com>
2024-09-24 07:08:32 -0800

    fix: fix route structure

--- Projects/Project_1/Back/src/routes/routes.js
+++ Projects/Project_1/Back/src/routes/routes.js
@@ -1,11 +1,8 @@
-import { createData } from '../utils/createData.js';
-import { getDataById } from '../utils/getDataById.js';
-import { updateDataById } from '../utils/updateDataById.js';
-import { deleteDataById } from '../utils/deleteDataById.js';
+import handlers from '../handler/index.js';
 
 export default async function routes(fastify) {
-    fastify.post('/api/data', createData);
-    fastify.get('/api/data/:id', getDataById);
-    fastify.put('/api/data/:id', updateDataById);
-    fastify.delete('/api/data/:id', deleteDataById);
+    fastify.post('/api/data', handlers.createData);
+    fastify.get('/api/data/:id', handlers.getDataById);
+    fastify.put('/api/data/:id', handlers.updateDataById);
+    fastify.delete('/api/data/:id', handlers.deleteDataById);
 }
